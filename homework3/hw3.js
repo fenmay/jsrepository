777d2b780c7b5eb3cdded30d830931d030a5e489
fenmay <mayer.mayer.real@yandex.ru>
2022-03-14 21:18:00 +0300

    initial commit

--- homework3/hw3.js
+++
@@ -1,9 +0,0 @@
-new Promise((resolve, reject) => {
-    setTimeout(()=> {
-        let number = console.log(Math.floor(Math.random() * 101)); 
-        if (number % 2 === 0) {
-            resolve(); 
-        } else reject();
-    }, 2000);
-}).then((number) => console.log('Resolve'))
-.catch((number) => console.log('Reject'));
---
+++ index.js
@@ -0,0 +1,3 @@
+import { sum } from './src/test.js';
+
+console.log(sum(2, 4));
