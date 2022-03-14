246e58a252e46a82de036cad6698d0eb22ab0d37
fenmay <mayer.mayer.real@yandex.ru>
2022-03-15 01:00:29 +0300

    fin

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-import { sum } from './src/test.js';
+const number = document.getElementById('input');
+const get = document.querySelector('.get');
+const clear = document.querySelector('.clear');
+const for_result = document.getElementById('for_result');
 
-console.log(sum(2, 4));
+let todos = [];
+
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(result => {
+      todos = result;
+      });
+
+get.onclick = render = () => {
+    const todo = todos.find(item => item.id == number.value);
+    const title = document.createElement('p');
+    const id = document.createElement('p');
+    const progress = document.createElement('p');
+
+    title.innerText = 'Title: ' + todo.title;
+    id.innerText = 'Id: ' + todo.id;
+    progress.innerText = todo.completed ? 
+        'Completed' : 'In progress';
+    
+    for_result.append(title);
+    for_result.append(id);
+    for_result.append(progress);
+}
+
+clear.onclick = reverse = () => {
+    for_result.innerHTML = '';
+}
