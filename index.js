dfeec1123379881e98fea4f4b36f6a20336822c7
fenmay <mayer.mayer.real@yandex.ru>
2022-03-25 02:14:07 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,113 +1,118 @@
 import './src/styles/style.scss';
 
-// // import { initializeApp } from "firebase/app";
-// // import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// Это часы работы ресторана
+// Вам надо написать метод, который будет принимать день недели и время. 
+// И метод будем определять, сможете ли вы сделать заказ или нет. 
+// Учитывайте тот факт, что за пол часа до закрытия или 
+// перерыва заказ не может быть оформлен. Ну типа кухня закрывается
 
-// // import { FIREBASE_CONFIG, DB_URL } from "./src/api/api-config";
- 
+const openingHours = {
+    dayOfWeek: ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY'],
+    periods: [
+      [
+        {
+          from: '10:00',
+          to: '14:00'
+        },
+        {
+          from: '15:00',
+          to: '18:00'
+        },
+        {
+          from: '18:30',
+          to: '22:00'
+        }
+      ],
+      [
+        {
+          from: '10:00',
+          to: '14:00'
+        },
+        {
+          from: '15:00',
+          to: '18:00'
+        },
+        {
+          from: '18:30',
+          to: '22:00'
+        }
+      ],
+      [
+        {
+          from: '10:00',
+          to: '14:00'
+        },
+        {
+          from: '15:00',
+          to: '18:00'
+        },
+        {
+          from: '18:30',
+          to: '22:00'
+        }
+      ],
+      [
+        {
+          from: '10:00',
+          to: '14:00'
+        },
+        {
+          from: '15:00',
+          to: '18:00'
+        },
+        {
+          from: '18:30',
+          to: '22:00'
+        }
+      ],
+      [
+        {
+          from: '08:00',
+          to: '12:00'
+        },
+        {
+          from: '13:00',
+          to: '19:00'
+        },
+        {
+          from: '19:30',
+          to: '23:30'
+        }
+      ],
+    ]
+  };
 
-// // const app = initializeApp(FIREBASE_CONFIG);
-// // const auth = getAuth();
+const newDay = openingHours.dayOfWeek;
+const monThur = openingHours.periods[0];
+const fri = openingHours.periods[4];
 
-// // let mail = document.getElementById('mail');
-// // let password = document.getElementById('password');
-// // let sign = document.getElementById('sign');
+console.log(newDay);
+console.log(monThur);
+console.log(fri);
 
+let arrTime = [];
 
-// // sign.onclick = () => createUserWithEmailAndPassword(auth, mail.value, password.value)
-// //   .then((userCredential) => {
-// //     // Signed in 
-// //     const user = userCredential.user;
-// //     console.log(user);
-// //     // ...
-// //   })
-// //   .catch((error) => {
-// //     const errorCode = error.code;
-// //     const errorMessage = error.message;
-// //     // ..
-// //     console.log(error);
-// //   });
+const fourDays = Object.values(monThur);
+const strDays = Object.values(fourDays[0]) + ' ' + Object.values(fourDays[1]) + ' ' + Object.values(fourDays[2]);
+const newTimes = strDays.split(',').join();
+// const omg = newTimes.split('');
+// .join(' ');
+// .split(' ');
+console.log(newTimes);
 
-// // const createTodo = () => {
-// //   fetch(
-// //     `${DB_URL}/todos.json`,
-// //     {
-// //       method: 'POST',
-// //       body: JSON.stringify({
-// //         title: 'My todo 4',
-// //         description: 'Do smth 4'
-// //       })
-// //     }
-// //   )
-// //     .then(response => response.json())
-// //     .then(res => console.log('Response: ', res));
-// // }
-
-// // createTodo();
-
-// const getTodo = () => {
-//   fetch(`${DB_URL}/todos.json`)
-//   .then(res => res.json())
-//   .then(response => {
-//     const arr = Object.keys(response).map(key => {
-//       return {
-//         ...response[key],
-//         id: key
-//       };
-//     });
-
-//     console.log(arr);
-//   });
-  
+// let slicedHours = [];
+// for (let index = 0; index < newTimes.length; index++) {
+//     slicedHours = slicedHours + newTimes[index].slice(0, 2).split(',');
+    
 // }
+// console.log(slicedHours);
 
-// getTodo();
+// let goodTimes = slicedHours.split('');
+// console.log(goodTimes);
+// for (let index = 0; index < finallyFourDays.length; index++) {
+//     arrTime.push(finallyFourDays[index])
+    
+// }
+// console.log(arrTime);
 
-// // const updateTodo = () => {
-// //   fetch(
-// //     `${DB_URL}/todos/-MySloekn8qvP_FczoBc.json`,
-// //     {
-// //       method: 'PUT',
-// //       body: JSON.stringify({
-// //         title: 'Updated title',
-// //         description: 'Updated description',
-// //         testField: 'Hello world'
-// //       })
-// //     }
-// //   );
-// // }
 
-// // updateTodo();
-
-// // const deleteTodo = () => {
-// //   fetch(`${DB_URL}/todos/-MySloekn8qvP_FczoBc.json`,
-// //   {
-// //     method: 'DELETE'
-// //   });
-// // }
-
-// // deleteTodo();
-
-// //это были CRUD
-
-import { PATHNAMES, ROUTES } from './src/shared/constants/routes.js';
-import { signInHandler } from './src/components/sign-in/sign-in.js';
-import { getToken } from './src/shared/services/local-storage-service';
-
-window.onload = () => {
-    const pathname = window.location.pathname;
-
-    switch (pathname) {
-        case PATHNAMES.home:
-            window.location.href = ROUTES.sign_in;
-            break;
-        case PATHNAMES.sign_in:
-            signInHandler();
-            break;
-        case PATHNAMES.main:
-            !getToken() ? window.location.href = ROUTES.sign_in : null;
-            break;
-        default:
-            break;
-    }
-}
