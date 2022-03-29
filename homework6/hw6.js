2451571432bedc80db0dec806b3bd19ed12267e7
fenmay <mayer.mayer.real@yandex.ru>
2022-03-29 14:53:38 +0300

    commit

--- homework6/hw6.js
+++ homework6/hw6.js
@@ -3,24 +3,16 @@ const get = document.querySelector('.get');
 const clear_res = document.querySelector('.clear_res');
 const clear_request = document.querySelector('.clear_request');
 const for_result = document.getElementById('for_result');
-const title = document.createElement('p');
-const id = document.createElement('p');
-const progress = document.createElement('p');
 const deleteTodo = document.createElement('button');
-const end = +number.value + 30;
 
-
-    
 get.onclick = () => {
     let todos = [];
-
-    for (index = +number.value; index < end; index++) {
+    
+    for (index = +number.value; index < (+number.value + 30); index++) {
         fetch(`https://jsonplaceholder.typicode.com/todos/${index}`)
         .then(response => response.json())
         .then(result => {
             todos = result;
-            console.log(todos);
-                
             title.innerText = 'TITLE: ' + todos.title + '; ';
             id.innerText = 'ID: ' + todos.id + '; ';
             progress.innerText = todos.completed ? 
@@ -28,18 +20,20 @@ get.onclick = () => {
             deleteTodo.className = 'delete_todo';
             deleteTodo.innerText = 'Delete Todo';
         });
-        for_result.append(title, id, progress, deleteTodo);
+        const title = document.createElement('p');
+        const id = document.createElement('p');
+        const progress = document.createElement('p');
+        const deleteTodo = document.createElement('button');
+        const finish = document.createElement('div');
+
+        finish.className = 'finish';
+        finish.append(title, id, progress, deleteTodo);
+        for_result.append(finish);
+
+        deleteTodo.onclick = () => finish.innerHTML = 'This Todo Is Deleted';
     }
 }
 
-clear_res.onclick = () => {
-    for_result.innerHTML = '';
-}
+clear_res.onclick = () => for_result.innerHTML = '';
 
-clear_request.onclick = () => {
-    number.value = '';
-}
-
-deleteTodo.onclick = () => {
-    for_result.innerText = '';
-}
+clear_request.onclick = () => number.value = '';
