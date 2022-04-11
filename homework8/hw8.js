01a35796250e3272c6d0625370aa6996e1177b28
fenmay <mayer.mayer.real@yandex.ru>
2022-04-11 22:49:58 +0300

    initial commit

--- homework8/hw8.js
+++ homework8/hw8.js
@@ -5,19 +5,28 @@ const calculate = document.getElementById('calculate');
 const clear = document.getElementById('clear');
 const result = document.getElementById('result');
 
+const noValue = () => result.innerHTML = 'Your result: error! You did not choose an action!';
+const plus = (x, y) => (+x.value) + (+y.value);
+const minus = (x, y) => (+x.value) - (+y.value);
+const multiply = (x, y) => (+x.value) * (+y.value);
+const divide = (x, y) => (+x.value) / (+y.value);
+const power = (x, y) => Math.pow((+x.value), (+y.value));
+const squareRoot = (x) => Math.sqrt((+x.value));
+
 calculate.onclick = () => {
   const calc = new Map([
-    ['noValue', result.innerHTML = 'error! You did not choose an action!'],
-    ['plus',  (+x.value) + (+y.value)],
-    ['minus',  (+x.value) - (+y.value)],
-    ['multiply',  (+x.value) * (+y.value)],
-    ['divide',  (+x.value) / (+y.value)],
-    ['power',  Math.pow((+x.value), (+y.value))],
-    ['squareRoot',  Math.sqrt((+x.value))]
+    ['noValue', noValue],
+    ['plus',  plus],
+    ['minus',  minus],
+    ['multiply',  multiply],
+    ['divide',  divide],
+    ['power',  power],
+    ['squareRoot',  squareRoot]
   ]);
 
   result.innerHTML = '';
-  result.append('Your result: ' + calc.get(chooseAction.value));
+  chooseAction.value === 'noValue' ? 
+    noValue() : result.innerHTML = 'Your result: ' + calc.get(chooseAction.value)(x, y);
 };
 
 clear.onclick = () => result.innerHTML = '';
