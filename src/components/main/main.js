30a54cc82030f7346190d56720be708f6be22949
fenmay <mayer.mayer.real@yandex.ru>
2022-05-01 23:06:45 +0300

    commit

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,37 +1,21 @@
-import { getUser, clearUser, clearToken } from '../../shared/services/local-storage-service';
 import { getTodos } from '../../api/api-handlers';
 import { Todo } from '../todo/todo';
-import { ROUTES } from '../../shared/constants/routes';
+import { Header } from '../header/header';
 
 export const mainPageHandler = async () => {
-    const headerUserName = document.getElementById('userName');
-    const headerEmail = document.getElementById('email');
-    const todoWrapper = document.querySelector('.main__todos')
-    const { firstName, lastName, email } = getUser();
-    const logoutBtn = document.getElementById('logout');
-    const findBtn = document.getElementById('find-user');
-    let todos = [];
-
-    // headerEmail.innerText = email;
-    // headerUserName.innerText = `${firstName} ${lastName}`;
-
-    await getTodos().then(todosArr => {
-      todos = Object.keys(todosArr).map(key => {
-        const todo = {id: key, ...todosArr[key]};
-
-        todoWrapper.append(new Todo(todo).getTodo());
-
-        return todo;
-      });
-    });
-
-    logoutBtn.onclick = () => {
-      clearUser();
-      clearToken();
-      window.location.href = ROUTES.sign_in;
-    };
+  const mainPage = document.querySelector('.main');
+  const todoWrapper = document.querySelector('.main__todos');
+  let todos = [];
     
-    findBtn.onclick = () => {
-      window.location.href = ROUTES.find-users;
-    };
-  };
+  Header.getHeader(mainPage);
+
+  await getTodos().then(todosArr => {
+    todos = Object.keys(todosArr).map(key => {
+      const todo = {id: key, ...todosArr[key]};
+
+      todoWrapper.append(new Todo(todo).getTodo());
+
+      return todo;
+    });
+  });
+};
